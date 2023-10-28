2b507a6b91143f954fd45e6a3e8be02ae0e8edc1
destantas <destan.tastan@gmail.com>
2023-10-28 22:19:20 +0300

    added score tracker and win screen

--- script.js
+++ script.js
@@ -1,69 +1,76 @@
 let playerScore = 0;
 let computerScore = 0;
-let result;
+let result = ``;
 let playerSelection;
 let computerSelection;
 
 function getComputerChoice() {
-  let choices = ["rock", "scissors", "paper"];
+  const choices = ["rock", "scissors", "paper"];
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function computerWins() {
+  computerScore++;
+  result = `Computer takes the round!`;
+}
+
+function playerWins() {
+  playerScore++;
+  result = `Player takes the round!`;
 }
 
 function playRound() {
   computerSelection = getComputerChoice();
 
-  function computerWins() {
-    computerScore++;
-    result = `Computer wins! ${computerScore}`; 
-  }
-  function playerWins() {
-    playerScore++;
-    result = `Player wins! ${playerScore}`;
+  if (playerSelection === computerSelection) {
+    result = "It's a tie!";
+  } else if (playerSelection === "rock") {
+    computerSelection === "paper" ? computerWins() : playerWins();
+  } else if (playerSelection === "scissors") {
+    computerSelection === "rock" ? computerWins() : playerWins();
+  } else if (playerSelection === "paper") {
+    computerSelection === "scissors" ? computerWins() : playerWins();
+  } else {
+    result = "Error. Play again.";
   }
 
-  if (playerSelection === computerSelection) {
-    result = "It is tie";
-  } else if (playerSelection == "rock") {
-    if (computerSelection == "paper") {
-      computerWins();
-    } else {
-      playerWins();
-    }
-  } else if (playerSelection == "scissors") {
-    if (computerSelection == "rock") {
-      computerWins();
-    } else if (computerSelection == "paper") {
-      playerWins();
-    }
-  } else if (playerSelection == "paper") {
-    if (computerSelection == "scissors") {
-      computerWins();
-    } else if (computerSelection == "rock") {
-      playerWins();
-    }
-  } else {
-    result = "error. Play again";
-  }
-  console.log(result);
+  updateScore();
+  checkGameOver();
 }
 
-let rockButton = document.body.appendChild(document.createElement("button"));
-let scissorsButton = document.body.appendChild(document.createElement("button"));
-let paperButton = document.body.appendChild(document.createElement("button"));
+function updateScore() {
+  scoreText.innerHTML = `The result is: ${result} <br> Player: ${playerScore} <br> Computer: ${computerScore}`;
+}
 
-rockButton.innerText = "Rock"
-scissorsButton.innerText = "Scissors"
-paperButton.innerText = "Paper"
+function checkGameOver() {
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore === 5) {
+      scoreText.innerHTML = `PLAYER WON!`;
+    } else {
+      scoreText.innerHTML = `COMPUTER WON!`;
+    }
+    disableButtons();
+  }
+}
 
-rockButton.addEventListener("click", function() {
-  playerSelection = 'rock'
-  playRound();
-})
-paperButton.addEventListener("click", function() {
-  playerSelection = 'paper'
-  playRound();
-})
-scissorsButton.addEventListener("click", function() {
-  playerSelection = 'scissors'
-  playRound();
-})
+function disableButtons() {
+  rockButton.disabled = true;
+  scissorsButton.disabled = true;
+  paperButton.disabled = true;
+}
+
+const rockButton = createButton("Rock", "rock");
+const scissorsButton = createButton("Scissors", "scissors");
+const paperButton = createButton("Paper", "paper");
+const scoreDiv = document.body.appendChild(document.createElement("div"));
+const scoreText = scoreDiv.appendChild(document.createElement("p"));
+
+function createButton(text, type) {
+  const btn = document.body.appendChild(document.createElement("button"));
+  btn.innerText = text;
+  btn.addEventListener("click", function () {
+    playerSelection = type;
+    playRound();
+  });
+  return btn;
+}
